b2a881fa5081319a510f626d2abee1df73138bda
HUH <102580192+Highly-Unstable-Hadron@users.noreply.github.com>
2025-07-10 15:18:30 +0530

    audhixhew

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import Integer, String, ForeignKey, PickleType, Double
 from sqlalchemy.ext.mutable import MutableList, MutableSet
 import time
+import toml
 import secrets
 
 app = Flask(__name__)
-app.secret_key = secrets.token_hex()
+app.secret_key = toml.load('secrets.toml')['SECRET'] #secrets.token_hex()
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 db = SQLAlchemy()
 db.init_app(app)
@@ -26,11 +27,18 @@ class GiveAway(db.Model):
     produceType: Mapped[str] = mapped_column(String, nullable=False)
     quantity: Mapped[str] = mapped_column(String, nullable=False)
 
+class Recipient(db.Model):
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    location: Mapped[str] = mapped_column(String)
+    passwordHash: Mapped[str] = mapped_column(String, nullable=False)
+    phoneNo: Mapped[int] = mapped_column(Integer, nullable=False)
+    email: Mapped[str] = mapped_column(String, nullable=False, primary_key=True)
+
 @app.route("/")
 def index():
     return render_template('index.html')
 
-@app.route("/farmer", methods=['GET', 'POST'])
+@app.route("/farmer-sign-up", methods=['GET', 'POST'])
 def farmer_sign_up_page():
     if request.method == 'GET':
         return render_template('farmersignup.html')  # TODO
@@ -43,6 +51,20 @@ def farmer_sign_up_page():
     session['email'] = request.form['email']
     session['type'] = 'FARMER'
     return redirect("/want-to-give-away")
+
+@app.route("/recipient-sign-up", methods=['GET', 'POST'])
+def recipient_sign_up_page():
+    if request.method == 'GET':
+        return render_template('recipientsignup.html')  # TODO
+    x = request.form.to_dict()
+    # x.pop("submit")
+    password_hash = generate_password_hash(x.pop('password'))
+    x['phoneNo'] = int(x['phoneNo'])
+    db.session.add(Recipient(passwordHash=password_hash, **x))
+    db.session.commit()
+    session['email'] = request.form['email']
+    session['type'] = 'RECP'
+    return redirect("/giveaways")
 
 @app.route("/want-to-give-away", methods=['GET', 'POST'])
 def give_away_form():
@@ -60,38 +82,39 @@ def give_away_form():
 @app.route("/login-farmer", methods=['GET', 'POST'])
 def login_farmer():
     if request.method == 'GET':
-        return render_template('')  # TODO
-    farmer = db.session.get(Farmer, request.form['email'])
-    if check_password_hash(farmer.passwordHash, request.form['password']):
+        return render_template('login.html')
+    farmer = db.session.execute(db.select(Farmer.passwordHash).where(Farmer.email == request.form['name'])).scalar_one_or_none()
+    if check_password_hash(farmer, request.form['password']):
         session['email'] = request.form['email']
         session['type'] = 'FARMER'
-        return redirect("/")
-    else:
-        return jsonify(error="Wrong"), 400
+        return jsonify(), 200
+    return jsonify(error="Wrong"), 401
     
 @app.route("/login-recipient", methods=['GET', 'POST'])
 def login_recipient():
     if request.method == 'GET':
-        return render_template('')  # TODO
-    farmer = db.session.get(Farmer, request.form['email'])
+        return render_template('login.html')
+    farmer = db.session.get(Recipient, request.form['name'])
     if check_password_hash(farmer.passwordHash, request.form['password']):
         session['email'] = request.form['email']
-        session['type'] = 'FARMER'
+        session['type'] = 'RECP'
         return redirect("/")
     else:
         return jsonify(error="Wrong"), 400
     
 @app.route("/giveaways")
 def giveaway_list():
-    glist = db.session.execute(db.select(GiveAway, Farmer.name, Farmer.location).where(GiveAway.farmer == Farmer.email)).all()
-
+    glist = db.session.execute(db.select(GiveAway, Farmer.name, Farmer.location, Farmer.phoneNo).where(GiveAway.farmer == Farmer.email)).all()
     return render_template("giveaways.html", glist=glist)
 
 @app.route("/my-giveaways")
 def my_giveaway_list():
     email = session.get('email')
-    glist = db.session.execute(db.select(GiveAway, Farmer.name, Farmer.location).where(GiveAway.farmer == Farmer.email)).all()
-    return render_template("giveaways.html", glist=glist)
+    glist = db.session.execute(
+        db.select(GiveAway).where(GiveAway.farmer == email)).fetchall()
+    glist2 = db.session.execute(db.select(Farmer).where(Farmer.email == email)).fetchone()
+    glist = map(lambda el: el[0], glist)
+    return render_template("mygiveaways.html", glist=glist, glist2=glist2[0])
     
 with app.app_context():
     db.create_all()
